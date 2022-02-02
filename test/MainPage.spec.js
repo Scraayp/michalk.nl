fc6f0f6e56fedcc2c801505f722b63aa7a967251
Michal <scraayp@gmail.com>
2022-02-02 08:57:27 +0000

    better testing

--- test/MainPage.spec.js
+++ test/MainPage.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import NuxtLogo from '@/components/NuxtLogo.vue'
+import Page from '@/pages/index.vue'
 
-describe('NuxtLogo', () => {
+describe('Page', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
+    const wrapper = mount(Page)
     expect(wrapper.vm).toBeTruthy()
   })
 })
