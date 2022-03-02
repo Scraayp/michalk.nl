40e6f785f3e9add164684c9eb11b2e8012df3e78
Scraayp <scraayp@gmail.com>
2022-03-02 12:56:15 +0100

    idk trying new things out

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <h1>Hello</h1>
+    </div>
   </main>
 </template>
