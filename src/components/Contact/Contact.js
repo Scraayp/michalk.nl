bd8d76ae03f65e07b702c4b38623c7524cb6fdf3
Scraayp <scraayp@gmail.com>
2022-09-01 12:00:00 +0200

    Start on form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,10 +10,43 @@ function Home() {
         <Container className="contact-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 className="heading">
-                Hi there.
-                <br /> Here is the contact form:
-              </h1>
+              <h1 className="heading">Hi there.</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={7} className="contact-form">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    placeholder="Enter your name"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="message">Message</label>
+                  <textarea
+                    className="form-control"
+                    id="message"
+                    rows="3"
+                    placeholder="Enter your message"
+                  ></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
             </Col>
           </Row>
         </Container>
