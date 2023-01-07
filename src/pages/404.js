56912a8c753ba39dc18ecf064b5181f48fb4af2f
Michal <scraayp@gmail.com>
2023-01-07 16:23:02 +0100

    Add custom 404.js page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,27 +1,30 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
 
 const pageStyles = {
-  color: "#232129",
+  color: "#fff",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 
 const paragraphStyles = {
   marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+};
+
+const linkStyle = {
+  color: "#fff",
+  // Remove the underlin from the link
+  textDecoration: "none",
+  // Add unbderline on hover
+  ":hover": {
+    textDecoration: "underline",
+  },
+};
 
 const NotFoundPage = () => {
   return (
@@ -30,20 +33,16 @@ const NotFoundPage = () => {
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
         <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
         <br />
-        <Link to="/">Go home</Link>.
+        <Link to="/" style={linkStyle}>
+          Go home
+        </Link>
+        .
       </p>
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Not found</title>;
