9a2e9beb259e854ac68480739af2041fa4e23236
Michal <scraayp@gmail.com>
2023-01-07 22:26:50 +0100

    Start new page

---
+++ src/pages/contact.js
@@ -0,0 +1,26 @@
+// Import react
+import * as React from "react";
+
+// Import the styling
+import "../styles/index.css";
+
+const ContactPage = () => {
+  return (
+    <h1 className="text-red">Test</h1>
+  );
+};
+
+export default ContactPage;
+
+export const Head = () => (
+  <>
+    <title>Construction</title>
+    <meta
+      name="description"
+      content="The website is under construction Soon!"
+    />
+    <meta charSet="utf-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </>
+);
