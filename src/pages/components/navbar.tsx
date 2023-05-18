fa1581e03a5d443123f9512c8d20261c91db15bd
Michal <scraayp@gmail.com>
2023-05-18 21:18:14 +0200

    Start with the footer

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -6,17 +6,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Navbar() {
   return (
-    <header className="text-white body-font">
-    <div className="container flex">
-      <a className="">
-          <Image src="/logo.png" alt="logo" width={80} height={80} className='rounded-full' />
-      </a>
-      <nav id="links" className="">
-        <Link className="mr-5 hover:text-primary" href="#">About</Link>
-        <Link className="mr-5 hover:text-primary" href="#">Projects</Link>
-        <Link className="mr-5 hover:text-primary" href="#"> Contact</Link>
-      </nav>
-    </div>
-  </header>
+   <header className="text-white body-font">
+        <div className="container flex">
+            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 float-left">
+                <Image src="/logo.png" alt="logo" width={80} height={80} className='rounded-full' />
+            </a>
+            <nav id="links" className=" float-right mr-auto">
+              <Link className="mr-5 hover:text-primary" href="#">About</Link>
+              <Link className="mr-5 hover:text-primary" href="#">Projects</Link>
+              <Link className="mr-5 hover:text-primary" href="#"> Contact</Link>
+            </nav>
+        </div>
+    </header>
   )
 }
