72b0ead031649ce8b94f223bfe981e165e8b6778
Michal <scraayp@gmail.com>
2023-06-07 22:02:15 +0200

    Fix white mode navbar

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -8,11 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   return (
-    <header className="text-white font-bold">
+    <header className="text-black dark:text-white font-bold">
       <div className="DESKTOPNAV space-x-8 hidden lg:flex">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link
-            className="flex title-font font-medium items-center text-white hover:text-primary mb-4 md:mb-0"
+            className="flex title-font font-medium items-center text-black dark:text-white hover:text-primary mb-4 md:mb-0"
             href="../"
           >
             <Image
@@ -25,13 +25,22 @@ export default function Navbar() {
             <span className="font-bold ml-3 text-xl">Michal</span>
           </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link className="mr-5 text-xl hover:text-primary" href="#about">
+            <Link
+              className="mr-5 text-xl text-black dark:text-white hover:text-primary"
+              href="#about"
+            >
               About
             </Link>
-            <Link className="mr-5 text-xl hover:text-primary" href="#projects">
+            <Link
+              className="mr-5 text-xl text-black dark:text-white hover:text-primary"
+              href="#projects"
+            >
               Portfolio
             </Link>
-            <Link className="mr-5 text-xl hover:text-primary" href="#contact">
+            <Link
+              className="mr-5 text-xl text-black dark:text-white hover:text-primary"
+              href="#contact"
+            >
               Contact
             </Link>
           </nav>
@@ -39,14 +48,16 @@ export default function Navbar() {
       </div>
       <div id="mobilenav">
         <section className="MOBILE-MENU flex lg:hidden">
-          <div className={isNavOpen ? "hidden" : "w-full grid justify-end mr-4"}>
+          <div
+            className={isNavOpen ? "hidden" : "w-full grid justify-end mr-4"}
+          >
             <div
               className="HAMBURGER-ICON space-y-2 mt-8 ml-8 right-0 w-full"
               onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
             >
-              <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-black dark:bg-white"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-black dark:bg-white"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-black dark:bg-white"></span>
             </div>
           </div>
 
@@ -56,7 +67,7 @@ export default function Navbar() {
               onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
             >
               <svg
-                className="h-8 w-8 text-white"
+                className="h-8 w-8 text-black dark:text-white"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
