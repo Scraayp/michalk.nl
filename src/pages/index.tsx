937fcac19fa1bcecf6104ddb751eaa2e58ed52e6
Michal <scraayp@gmail.com>
2023-06-08 17:50:07 +0200

    Finished the about me section, started working on projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +13,44 @@ export default function Home() {
           <span className="text-primary">Hey!</span> I am Michal,
         </h1>
         <h2 className="text-3xl font-bold text-center mt-10 text-black dark:text-white">
-          I build the <span className="text-primary">backend</span> side of apps and websites.
+          I build the <span className="text-primary">backend</span> side of apps
+          and websites.
         </h2>
       </div>
-      <div id="about" className="m-auto w-head mt-32">
-        <h1 className="text-left text-4xl font-bold text-primary">About</h1>
+      <div id="about" className="m-auto w-head mt-60 block h-64">
+        <h1 className="text-left text-4xl font-bold text-primary block">
+          About
+        </h1>
+        <p className="mt-5 font-semibold text-lg inline-block text-black dark:text-white">
+          I am Michal a software developer from{" "}
+          <span className="text-primary">the Netherlands.</span> <br />I have
+          been coding since 2012 and have been in{" "}
+          <span className="text-primary">love</span> with it since. <br />
+          In my free time, I spend the most time on{" "}
+          <span className="text-primary">soccer</span>. <br />I am a player,
+          referee and a trainer at my local club.
+        </p>
+        <Image
+          src={"/me.jpg"}
+          alt="Yeah, that's me."
+          width={400}
+          height={400}
+          className="rounded-full right-0 float-right"
+        />
+      </div>
+      <div id="projects" className="m-auto w-head mt-96 mb-40">
+        <h1 className="text-left text-4xl font-bold text-primary block">
+          Projects
+        </h1>
+        <p className="mt-5 font-semibold text-lg inline-block text-black dark:text-white">
+          I am Michal a software developer from{" "}
+          <span className="text-primary">the Netherlands.</span> <br />I have
+          been coding since 2012 and have been in{" "}
+          <span className="text-primary">love</span> with it since. <br />
+          In my free time, I spend the most time on{" "}
+          <span className="text-primary">soccer</span>. <br />I am a player,
+          referee and a trainer at my local club.
+        </p>
       </div>
     </main>
   );
