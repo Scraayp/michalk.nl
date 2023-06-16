01f23d6aa775139b39097476b013d16b5a6fa4ff
Michal <scraayp@gmail.com>
2023-06-16 15:47:57 +0200

    Added the cv page

---
+++ src/pages/cv.tsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import Navbar from "./components/navbar";
+import Image from "next/image";
+import React from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <div>
+        <embed
+          id="pdf-embed"
+          type="application/pdf"
+          width="100%"
+          height="600px"
+          src="/cv.pdf"
+        />
+      </div>
+    </main>
+  );
+}
