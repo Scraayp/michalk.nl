863a10291a2ce5d3d693c64169cbcc80a41d1c93
Michal <scraayp@gmail.com>
2023-06-18 16:20:09 +0200

    Ues the script srcs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="bg-lightbg dark:bg-darkbg">
-        <script src="https://kit.fontawesome.com/30c1fe66c2.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+        <Script src="https://kit.fontawesome.com/30c1fe66c2.js"></Script>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></Script>
         <Main />
         <NextScript />
       </body>
