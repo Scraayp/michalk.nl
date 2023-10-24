5f0f32cca09a6f5cb59052a418ac8ed15e246eb5
Michal <hello@michalk.nl>
2023-10-24 16:35:32 +0200

    start with v2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,35 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body className="bg-lightbg dark:bg-darkbg">
         <Main />
         <NextScript />
+        <Script src="https://kit.fontawesome.com/30c1fe66c2.js"></Script>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/flowbite.min.js"></Script>
+        <Script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js" />
       </body>
     </Html>
   );
