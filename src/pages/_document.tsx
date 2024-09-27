32383c0219a767609d81017bd952e3ce2aae5833
Michal <hello@michalk.nl>
2024-09-27 10:04:34 +0200

    feat: Add Google Analytics script
    Add the necessary script code for Google Analytics to track website traffic.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,23 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="bg-lightbg dark:bg-darkbg">
+        {/* Add the script code */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-G9V2N9XES1"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G9V2N9XES1');
+            `,
+          }}
+        />
         <Main />
         <NextScript />
         <Analytics />
