93fb3de119877b3663e6c59fa023b9974724a4a0
Michal <hello@michalk.nl>
2024-09-27 09:54:38 +0200

    (feat) Added vercel analytics.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import ReactGA from "react-ga4";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   ReactGA.initialize("G-G9V2N9XES1");
+  Analytics({ mode: "production", debug: true });
   return (
     <Html lang="en">
       <Head />
